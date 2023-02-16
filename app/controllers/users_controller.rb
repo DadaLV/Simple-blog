ed291f2d821bb0670e2cc108c630df0ed97f215d
DadaLV <vynohradova.l.a@gmail.com>
2023-02-16 12:37:28 +0200

    daa users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,12 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = "Welcome to the Simple Blog, #{@user.username}, you have successfully signed up"
-      redirect_to articles_path
-    else  
-      render "new"
-    end
+  def new
+    @user = User.new
   end
 
   def edit
@@ -28,10 +23,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Simple Blog, #{@user.username}, you have successfully signed up"
+      redirect_to articles_path
+    else  
+      render "new"
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
 
 end
